9323744f8d713555afcca0e98612eaa46af1ad38
JangMyungJoo <wkdaudwn11@naver.com>
2019-08-03 10:31:38 +0900

    GlobalStyles

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Router from "./Router";
+import GlobalStyles from './GlobalStyles';
 
 export default class App extends Component {
   render() {
     return (
       <>
         <Router />
+        <GlobalStyles />
       </>
     )
   }
