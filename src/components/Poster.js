62ceae276db65172b85a06ac2cfcd32a1edc8f8a
JangMyungJoo <wkdaudwn11@naver.com>
2019-08-09 21:56:20 +0900

    Poster
    포스터 화면 꾸미기

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -3,24 +3,67 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
-const Containter = styled.div``;
-const ImageContainter = styled.div``;
-const Image = styled.div``;
-const Rating = styled.span``;
-const Title = styled.span``;
-const Year = styled.span``;
+const Containter = styled.div`
+    font-size: 12px;
+`;
+
+const Image = styled.div`
+    background-image: url()(${props => props.bgUrl});
+    height: 100px;
+    background-size: cover;
+    border-radius: 4px;
+    background-position: center center;
+    transition: opacity 0.1s linear;
+`;
+
+const Rating = styled.span`
+    bottom: 5px;
+    right: 5px;
+    position:  absolute;
+    opacity: 0;
+    transition: opacity 0.1s linear;
+`;
+
+const ImageContainter = styled.div`
+    margin-bottom: 5px;
+    position: relative;
+    &:hover {
+        ${Image} {
+            opacity: 0.3;
+        }
+        ${Rating} {
+            opacity: 1;
+        }
+    }
+`;
+
+const Title = styled.span`
+    display: block;
+    margin-bottom: 3px;
+`;
+
+const Year = styled.span`
+    font-size: 10px;
+    color: rgba(255, 255, 255, 0.5);
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie }) => (
     <Link to={ isMovie ? `/movie/${id}` : `/show/${id}` }>
         <Containter>
             <ImageContainter>
-                <Image bgurl={imageUrl} />
+                <Image bgurl={
+                    imageUrl
+                    ? `https://image.tmdb.org/t/p/w300${imageUrl}`
+                    : require('./assets/noPosterSmall.png')
+                } />
                 <Rating>
                     <span role="img" aria-label="rating">⭐</span>
                     {"  "}
                     {rating}/10
                 </Rating>
-                <Title>{title}</Title>
+                <Title>
+                    {title.length > 18 ? `${title.substring(0,18)}...` : title}
+                </Title>
                 <Year>{year}</Year>
             </ImageContainter>
         </Containter>
