695a8485ac7a4bf9b649f509bb3bb75f18a70eac
JangMyungJoo <wkdaudwn11@naver.com>
2019-08-09 21:02:36 +0900

    Add loader component && movie, tv presenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -2,7 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => null;
+import Section from '../../components/Section';
+import Loader from '../../components/Loader';
+
+const Container = styled.div`
+    padding: 0px 10px;
+`;
+
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => (
+    loading ? (
+        <Loader />
+    ) : (
+        <Container>
+            {topRated && topRated.length > 0 && (
+                <Section title="TopRated">
+                    { topRated.map(tv => (
+                        <span key={tv.id}>{tv.title}</span>
+                    ))}
+                </Section>
+            )}
+
+            {popular && popular.length > 0 && (
+                <Section title="Popular TV">
+                    { popular.map(tv => (
+                        <span key={tv.id}>{tv.title}</span>
+                    ))}
+                </Section>
+            )}
+
+            {airingToday && airingToday.length > 0 && (
+                <Section title="AiringToday TV">
+                    { airingToday.map(tv => (
+                        <span key={tv.id}>{tv.title}</span>
+                    ))}
+                </Section>
+            )}
+        </Container>
+    )
+);
 
 TVPresenter.propTypes = {
     topRated: PropTypes.array,
